8968c4c56a1125f9ec88130a6d23b41dffd7262a
KimA-Mik <ananim38@gmail.com>
2022-11-03 17:15:42 +0300

    Some models playing

--- Prototype/Store/models.py
+++ Prototype/Store/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class GameItem(models.Model):
+    id = models.Index
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=512)
+    release_date = models.DateField("release date")
